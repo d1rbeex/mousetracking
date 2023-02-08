5ff5e0fce3b8409c65e364d4843c8c71335dfadb
d1rbeex <124755758+d1rbeex@users.noreply.github.com>
2023-02-08 08:56:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+addEventListener("mousemove", (e) => {
+    let circle = document.createElement("div");
+    circle.className = "circle";
+    circle.style.left = (event.pageX - 12) + "px";
+    circle.style.top = (event.pageY - 12) + "px";
+    document.body.appendChild(circle);
+
+    setTimeout(() => {
+        circle.remove()
+    }, 10);
+});
